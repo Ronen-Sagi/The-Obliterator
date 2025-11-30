c18128b34c5ca1adaf46a74b487a89ec677fb2a6
ronench20 <ronencher@gmail.com>
2025-11-30 21:33:42 +0200

    add movment

---
+++ Assets/Scripts/Movment.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movment : MonoBehaviour
+{
+    [SerializeField] private InputAction up = new InputAction(type: InputActionType.Button);
+    [SerializeField] private InputAction down = new InputAction(type: InputActionType.Button);
+    [SerializeField] private InputAction left = new InputAction(type: InputActionType.Button);
+    [SerializeField] private InputAction right = new InputAction(type: InputActionType.Button);
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float maxMoveSpeed = 5f;
+    [SerializeField] private float friction = 5f;
+    
+    private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        up.AddBinding("<Keyboard>/w");
+        down.AddBinding("<Keyboard>/s");
+        left.AddBinding("<Keyboard>/a");
+        right.AddBinding("<Keyboard>/d");
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+    
+    private void OnEnable()
+    {
+        up.Enable();
+        down.Enable();
+        left.Enable();
+        right.Enable();
+    }
+    
+    private void OnDisable()
+    {
+        up.Disable();
+        down.Disable();
+        left.Disable();
+        right.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = Vector2.Lerp(rb.linearVelocity, Vector2.zero, friction * Time.deltaTime);
+        if (up.IsPressed() && rb.linearVelocity.y < maxMoveSpeed)
+        {
+            rb.AddForce(Vector2.up * moveSpeed, ForceMode2D.Impulse);
+        }
+        if (down.IsPressed() && rb.linearVelocity.y > -maxMoveSpeed)
+        {
+            rb.AddForce(Vector2.down * moveSpeed, ForceMode2D.Impulse);
+        }
+        if (left.IsPressed() && rb.linearVelocity.x > -maxMoveSpeed)
+        {
+            rb.AddForce(Vector2.left * moveSpeed, ForceMode2D.Impulse);
+        }
+        if (right.IsPressed() && rb.linearVelocity.x < maxMoveSpeed)
+        {
+            rb.AddForce(Vector2.right * moveSpeed, ForceMode2D.Impulse);
+        }
+        
+    }
+
+
+}
