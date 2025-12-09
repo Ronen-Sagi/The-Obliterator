b45435123e59a4a00104f148278b36c0359477a7
ronench20 <ronencher@gmail.com>
2025-12-09 12:43:55 +0200

    linter

--- Assets/Scripts/Enemy/EnemyScript.cs
+++ Assets/Scripts/Enemy/EnemyScript.cs
@@ -4,16 +4,16 @@ public class EnemyScript : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other. CompareTag("Bullet"))
+        if (other.CompareTag("Bullet"))
         {
             TutorialKillCounter killCounter = FindObjectOfType<TutorialKillCounter>();
             if (killCounter != null)
             {
                 killCounter.OnEnemyKilled();
             }
-        
+
             Destroy(gameObject);
-            Destroy(other. gameObject);
+            Destroy(other.gameObject);
         }
     }
 }
--- Assets/Scripts/Player/Movment.cs
+++ Assets/Scripts/Player/Movment.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using UnityEngine. InputSystem;
+using UnityEngine.InputSystem;
 
 public class Movment : MonoBehaviour
 {
@@ -21,11 +21,11 @@ public class Movment : MonoBehaviour
     void Start()
     {
         up.AddBinding("<Keyboard>/w");
-        down. AddBinding("<Keyboard>/s");
+        down.AddBinding("<Keyboard>/s");
         left.AddBinding("<Keyboard>/a");
-        right. AddBinding("<Keyboard>/d");
+        right.AddBinding("<Keyboard>/d");
         rb = GetComponent<Rigidbody2D>();
-        
+
         originalMoveSpeed = moveSpeed;
         originalMaxMoveSpeed = maxMoveSpeed;
     }
@@ -33,15 +33,15 @@ public class Movment : MonoBehaviour
     private void OnEnable()
     {
         up.Enable();
-        down. Enable();
+        down.Enable();
         left.Enable();
         right.Enable();
     }
 
     private void OnDisable()
     {
-        up. Disable();
-        down. Disable();
+        up.Disable();
+        down.Disable();
         left.Disable();
         right.Disable();
     }
@@ -49,25 +49,25 @@ public class Movment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb. linearVelocity = Vector2.Lerp(rb.linearVelocity, Vector2. zero, friction * Time.deltaTime);
+        rb.linearVelocity = Vector2.Lerp(rb.linearVelocity, Vector2.zero, friction * Time.deltaTime);
         if (up.IsPressed() && rb.linearVelocity.y < maxMoveSpeed)
         {
-            rb.AddForce(Vector2. up * moveSpeed, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * moveSpeed, ForceMode2D.Impulse);
         }
 
-        if (down. IsPressed() && rb.linearVelocity.y > -maxMoveSpeed)
+        if (down.IsPressed() && rb.linearVelocity.y > -maxMoveSpeed)
         {
             rb.AddForce(Vector2.down * moveSpeed, ForceMode2D.Impulse);
         }
 
-        if (left.IsPressed() && rb. linearVelocity.x > -maxMoveSpeed)
+        if (left.IsPressed() && rb.linearVelocity.x > -maxMoveSpeed)
         {
-            rb.AddForce(Vector2.left * moveSpeed, ForceMode2D. Impulse);
+            rb.AddForce(Vector2.left * moveSpeed, ForceMode2D.Impulse);
         }
 
-        if (right.IsPressed() && rb.linearVelocity. x < maxMoveSpeed)
+        if (right.IsPressed() && rb.linearVelocity.x < maxMoveSpeed)
         {
-            rb.AddForce(Vector2.right * moveSpeed, ForceMode2D. Impulse);
+            rb.AddForce(Vector2.right * moveSpeed, ForceMode2D.Impulse);
         }
     }
 
@@ -78,7 +78,7 @@ public class Movment : MonoBehaviour
             isSpeedBoosted = true;
             moveSpeed = originalMoveSpeed * speedBoostMultiplier;
             maxMoveSpeed = originalMaxMoveSpeed * speedBoostMultiplier;
-            
+
             Invoke(nameof(DeactivateSpeedBoost), duration);
         }
     }
