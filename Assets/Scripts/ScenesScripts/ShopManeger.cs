cde96b46ca35aea97cc4e54fe8fe39cd709f3a2f
ronench20 <ronencher@gmail.com>
2025-12-14 15:19:48 +0200

    shop kinda works

--- Assets/Scripts/ScenesScripts/ShopManeger.cs
+++ Assets/Scripts/ScenesScripts/ShopManeger.cs
@@ -6,6 +6,13 @@ public class ShopManeger : MonoBehaviour
     [SerializeField] private MoneyManeger moneyManeger;
     [SerializeField] private TextMeshProUGUI purchaseText;
     [SerializeField] private float textDuration = 2f;
+    
+    [SerializeField] private TextMeshProUGUI amountText;
+
+    [SerializeField] private int bulletPrice = 30;
+    [SerializeField] private int shieldPrice = 20;
+    [SerializeField] private int tiresPrice = 15;
+    [SerializeField] private int powerUpPrice = 50;
 
     private float textTimer = 0f;
 
@@ -18,12 +25,14 @@ public class ShopManeger : MonoBehaviour
         {
             purchaseText.gameObject.SetActive(false);
         }
+
+        UpdateAmountText();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isTextActive)
+        if (isTextActive)
         {
             textTimer -= Time.deltaTime;
             if (textTimer <= 0f)
@@ -32,18 +41,67 @@ public class ShopManeger : MonoBehaviour
                 isTextActive = false;
             }
         }
+
+        UpdateAmountText();
     }
 
     public void OnNewBulletPressed()
     {
-        if (moneyManeger.GetMoneyAmount() >= 20)
+        Debug.Log("bullet pressed, Money:  " + moneyManeger.GetMoneyAmount());
+        if (moneyManeger.GetMoneyAmount() >= bulletPrice)
         {
-            moneyManeger.ReduceMoneyAmount(20);
+            moneyManeger.ReduceMoneyAmount(bulletPrice);
             ShowPurchaseText("New bullet purchased");
+        }
+        else
+        {
+            ShowPurchaseText("Not enough money for new bullet");
         }
     }
 
-    /// Displays the purchase confirmation text for a short duration.
+    public void OnNewShieldPressed()
+    {
+        Debug.Log("shield pressed, Money:  " + moneyManeger.GetMoneyAmount());
+        if (moneyManeger.GetMoneyAmount() >= shieldPrice)
+        {
+            moneyManeger.ReduceMoneyAmount(shieldPrice);
+            ShowPurchaseText("New shield purchased");
+        }
+        else
+        {
+            ShowPurchaseText("Not enough money for new shield");
+        }
+    }
+
+    public void OnNewTirePressed()
+    {
+        Debug.Log("tires pressed, Money:  " + moneyManeger.GetMoneyAmount());
+        if (moneyManeger.GetMoneyAmount() >= tiresPrice)
+        {
+            moneyManeger.ReduceMoneyAmount(tiresPrice);
+            ShowPurchaseText("New tires purchased");
+        }
+        else
+        {
+            ShowPurchaseText("Not enough money for new tires");
+        }
+    }
+
+    public void OnNewPowerUpPressed()
+    {
+        Debug.Log("power-up pressed, Money:  " + moneyManeger.GetMoneyAmount());
+        if (moneyManeger.GetMoneyAmount() >= powerUpPrice)
+        {
+            moneyManeger.ReduceMoneyAmount(powerUpPrice);
+            ShowPurchaseText("New power-up purchased");
+        }
+        else
+        {
+            ShowPurchaseText("Not enough money for new power-up");
+        }
+    }
+
+    /// Displays the purchase confirmation text for a short duration. 
     private void ShowPurchaseText(string message)
     {
         if (purchaseText != null)
@@ -52,6 +110,14 @@ public class ShopManeger : MonoBehaviour
             purchaseText.gameObject.SetActive(true);
             textTimer = textDuration;
             isTextActive = true;
+        }
+    }
+    
+    public void UpdateAmountText()
+    {
+        if (amountText != null && moneyManeger != null)
+        {
+            amountText.text = "You have: " + moneyManeger.GetMoneyAmount().ToString() + "$";
         }
     }
 }
