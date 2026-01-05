0228c22647a9cc75546a2a64edd5fedf47b0b4df
ronench20 <ronencher@gmail.com>
2026-01-05 13:11:52 +0200

    coin sound

--- Assets/Scripts/Coins/CoinDrop.cs
+++ Assets/Scripts/Coins/CoinDrop.cs
@@ -2,11 +2,24 @@ using UnityEngine;
 
 public class CoinDrop : PickupDrop
 {
+    [SerializeField] private AudioClip coinSound;
     [SerializeField] private int coinAmount = 1;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     protected override void OnCollected(GameObject player)
     {
         if (MoneyManager.Instance != null)
+        {
             MoneyManager.Instance.AddMoneyAmount(coinAmount);
+
+            if (coinSound != null)
+                AudioSource.PlayClipAtPoint(coinSound, transform.position, 1f);
+        }
     }
 }
